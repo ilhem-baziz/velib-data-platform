d8af46469faadb4e7a9858bcababb048e926d4e6
ilhem-baziz <ilhembaziz92@gmail.com>
2026-07-01 02:28:22 +0200

    add Gold HDFS PostgreSQL and KPI history

--- dags/velib_gold_transformation.py
+++ dags/velib_gold_transformation.py
@@ -1,12 +1,7 @@
 from datetime import datetime, timedelta
-import sys
 
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-
-sys.path.append("/opt/airflow/src")
-
-from transform.silver_to_gold import run_silver_to_gold
+from airflow.operators.bash import BashOperator
 
 
 default_args = {
@@ -17,18 +12,32 @@ default_args = {
 }
 
 
+SPARK_SILVER_TO_GOLD_CMD = """
+spark-submit \
+  --master spark://spark-master:7077 \
+  --deploy-mode client \
+  --conf spark.driver.host=airflow-scheduler \
+  --conf spark.driver.bindAddress=0.0.0.0 \
+  --conf spark.hadoop.fs.defaultFS=hdfs://hdfs-namenode:9000 \
+  --conf spark.hadoop.dfs.replication=2 \
+  --conf spark.driverEnv.PYTHONPATH=/opt/airflow/src \
+  --conf spark.executorEnv.PYTHONPATH=/opt/app/src \
+  /opt/airflow/src/spark/silver_to_gold_spark.py
+"""
+
+
 with DAG(
     dag_id="velib_gold_transformation",
-    description="Construction des indicateurs Gold depuis Silver",
+    description="Transformation Silver HDFS vers Gold HDFS avec Spark",
     default_args=default_args,
     start_date=datetime(2026, 1, 1),
     schedule_interval=None,
     catchup=False,
     max_active_runs=1,
-    tags=["velib", "gold", "transformation"],
+    tags=["velib", "gold", "spark", "hdfs"],
 ) as dag:
 
-    task_silver_to_gold = PythonOperator(
-        task_id="construire_gold",
-        python_callable=run_silver_to_gold,
+    task_silver_to_gold = BashOperator(
+        task_id="spark_silver_vers_gold_hdfs",
+        bash_command=SPARK_SILVER_TO_GOLD_CMD,
     )
